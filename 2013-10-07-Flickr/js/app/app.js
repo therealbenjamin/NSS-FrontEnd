16b534d905e7ff138786b7beb2c19038356dd6eb
Benjamin Blackburn <benjamin.blackburn@gmail.com>
2013-10-07 11:39:53 -0500

    flickr lab

---
+++ 2013-10-07-Flickr/js/app/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+$(document).ready(initialize);
+
+function initialize(){
+  $(document).foundation();
+  $('#search').click(searchFlickr);
+}
+
+function searchFlickr(){
+  const API_KEY = 'f2b643a85999bc1ce3676579ac5420a4';
+  const PER_PAGE = 500;
+  var page = 1;
+
+  var query = $('#query').val();
+  var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&text=' + query + '&per_page=' + PER_PAGE + '&page=' + page + '&format=json&jsoncallback=?';
+  $.getJSON(url, results);
+}
+
+function results(data){
+  for(var i = 0; i < data.photos.photo.length; i++){
+    createImage(data.photos.photo[i]);
+  }
+}
+
+function createImage(photo){
+  var url = 'url(http://farm'+ photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_m.jpg)';
+  var $div = $('<div>');
+  $div.addClass('photo');
+  $div.css('background-image', url);
+  $('#photos').prepend($div);
+}
