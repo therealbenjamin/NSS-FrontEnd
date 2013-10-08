dd0c9cb2319831e4946a8b041ae34e90ed5cf14a
Benjamin Blackburn <benjamin.blackburn@gmail.com>
2013-10-08 09:51:42 -0500

    initial

--- 2013-10-08-Exception-Handling/js/app/app.js
+++ 2013-10-08-Exception-Handling/js/app/app.js
@@ -4,4 +4,14 @@ $(document).ready(initialize);
 
 function initialize(){
   $(document).foundation();
+
+  try {
+  console.log(y);
+  }
+
+  catch(e){
+    console.log('you just received the error: ' + e);
+  }
+
+
 }
