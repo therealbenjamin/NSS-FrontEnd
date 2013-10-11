685be10d6a4376e3d7fbdca261a0c12bbd663b1f
Benjamin Blackburn <benjamin.blackburn@gmail.com>
2013-10-11 10:13:57 -0500

    incorporated Amanda's changes

--- 2013-10-10-Stock-Market/js/app/app.js
+++ 2013-10-10-Stock-Market/js/app/app.js
@@ -9,6 +9,8 @@ var Δstatistics;
 var db = {};
 db.stocks = [];
 db.statistics = {};
+db.statistics.balance = 0;
+db.statistics.total = 0;
 
 
 $(document).ready(initialize);
@@ -17,17 +19,21 @@ function initialize(){
   $(document).foundation();
   $('#add_funds').click(setFunds);
   $('#buy').click(setStock);
+  $('#add_interval').click(setInterval);
 
   Δdb = new Firebase('https://stock-market-bb.firebaseio.com/');
   Δstocks = Δdb.child('stocks');
   Δstatistics = Δdb.child('statistics');
   Δstocks.on('child_added', stockAdded);
+  Δstatistics.on('value', balanceChanged);
 }
 
 function stockAdded(snapshot) {
   var stock = snapshot.val();
   addItem(stock);
+  getTotal(stock);
 }
+
 
 function getStockQuote(symbol, fn) {
   var data = {};
@@ -35,26 +41,20 @@ function getStockQuote(symbol, fn) {
   $.getJSON('http://dev.markitondemand.com/Api/Quote/jsonp?callback=?', data, fn); // jQuery.getJSON( url [, data ] [, success( data, textStatus, jqXHR ) ] )
 }
 
-function receiveQuote(data, textStatus, jqXHR) {
-  console.log(data);
-  console.log(textStatus);
-  console.log(jqXHR);
-}
-
 function setFunds() {
-  var funds = parseFloat($('#funds').val());
-  $('#create_funds').text('Current Funds: $' + funds);
-  $('#add_funds').hide();
-  $('#funds').hide();
+  db.statistics.balance = parseFloat($('#funds').val());
+  $('#balance').val('$' + db.statistics.balance);
+  Δstatistics.set(db.statistics);
 }
 
 function setStock() {
-  getStockQuote(symbol, function(data) {
+  getStockQuote(symbol, function(data){
     var stock = {};
 
     var symbol = data.Data.Symbol;
     var name = data.Data.Name;
-    var quote = parseFloat(data.Data.LastPrice);
+    var price = parseFloat(data.Data.LastPrice);
+    var quote = price.toFixed(2);
     var quantity = parseInt($('#quantity').val(), 10);
     var total = quote * quantity;
 
@@ -66,21 +66,34 @@ function setStock() {
 
     Δstocks.push(stock);
 
-    var balance = $('#create_funds').text();
-
+    db.statistics.balance -= total;
+    $('#balance').val('$' + db.statistics.balance);
   });
 }
 
 function addItem(stock) {
   var row = '<tr><td class="symbol"></td><td class="name"></td><td class="quote"></td><td class="purchased"></td><td class="total"></td></tr>';
   var $row = $(row);
+  var num = parseFloat(stock.total);
+  var n = num.toFixed(2);
 
   $row.children('.symbol').text(stock.symbol);
   $row.children('.name').text(stock.name);
   $row.children('.quote').text(stock.boughtPrice);
-
   $row.children('.purchased').text(stock.purchased);
-  $row.children('.total').text('$' + stock.total);
+  $row.children('.total').text('$' + n);
 
   $('#stock_list').append($row);
+}
+
+function balanceChanged(snapshot){
+  var statistics = snapshot.val();
+  db.statistics.balance = statistics.balance;
+  $('#balance').val('$' + db.statistics.balance);
+}
+
+function getTotal(stock){
+  db.statistics.total += (stock.boughtPrice * stock.purchased);
+  $('#total').val('$' + db.statistics.total);
+  Δstatistics.set(db.statistics);
 }
