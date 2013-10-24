ebe6ecdd38704cfac4e337df579ddaebd565342a
Benjamin Blackburn <benjamin.blackburn@gmail.com>
2013-10-24 09:04:55 -0500

    :neckbeard:

---
+++ 2013-10-22-Shopping-Site/public/js/tests/keys.js
@@ -0,0 +1,3 @@
+var db = {};
+db.keys = {};
+db.keys.firebase = 'https://shopping-site-test-bb.firebaseio.com/';
---
+++ 2013-10-22-Shopping-Site/public/js/tests/master.js
@@ -0,0 +1,115 @@
+'use strict';
+
+module('Integration Testing', {setup: setupTest, teardown: teardownTest});
+
+function setupTest(){
+  turnHandlersOff();
+  turnHandlersOn();
+  db.products = [];
+  db.customers = [];
+  db.orders = [];
+  db.pagination.currentPage = 1;
+  db.pagination.currentRowCount = 0;
+  Δdb.remove();
+}
+
+function teardownTest(){
+}
+
+test('Add Product', function(){
+  expect(11);
+
+  $('#product-image').val('ipad-air.png');
+  $('#product-name').val('Ipad Air');
+  $('#product-weight').val('1.0');
+  $('#product-price').val('500.00');
+  $('#product-off').val('10');
+  $('#add-product').trigger('click');
+
+  equal(db.products.length, 1, 'products array should have 1 element');
+  ok(db.products[0].id, 'id should be populated');
+  ok(db.products[0] instanceof Product, 'product should be an instanceof Product');
+  equal(db.products[0].image, 'ipad-air.png', 'product should have an image');
+  equal(db.products[0].name, 'Ipad Air', 'product should have a name');
+  equal(db.products[0].weight, 1.0, 'product should have a weight');
+  // QUnit.close(db.products[0].salePrice(), 450, 0.01, 'product should have a sale price');
+
+  equal($('#products tr').length, 2, 'should be 2 rows in table');
+  equal($('#products tr:nth-child(2) > td').length, 6, 'should be 6 columns in row');
+  equal($('#products .product-name').text(), 'Ipad Air', 'name column should be populated');
+  equal($('#products .product-sale').text(), '$450.00', 'sale column should be populated');
+  equal($('#products .product-image img').attr('src'), '/img/ipad-air.png', 'image column should be populated');
+});
+
+test('Product Pagination', function(){
+  expect(18);
+
+  for(var i = 0; i < 12; i++){
+    var name = Math.random().toString(36).substring(2);
+    var image = Math.random().toString(36).substring(2) + '.png';
+    var weight = Math.random() * 100;
+    var price = Math.random() * 1000;
+    var off = Math.random() * 100;
+
+    createTestProduct(name, image, weight, price, off);
+  }
+
+  equal(db.products.length, 12, 'should have 12 products');
+  equal(db.pagination.perPage, 5, 'should have 5 products per page');
+  equal(db.pagination.currentPage, 1, 'should be on page 1');
+  equal($('#products tr').length, 6, 'should have 5 products in table');
+  equal($('#previous.hidden').length, 1, 'previous button should be hidden');
+  equal($('#next:not(.hidden)').length, 1, 'next button should not be hidden');
+
+  $('#next').trigger('click');
+
+  equal(db.pagination.currentPage, 2, 'should be on page 2');
+  equal($('#products tr').length, 6, 'should have 5 products in table');
+  equal($('#previous:not(.hidden)').length, 1, 'previous button should not be hidden');
+  equal($('#next:not(.hidden)').length, 1, 'next button should not be hidden');
+
+  $('#next').trigger('click');
+
+  equal(db.pagination.currentPage, 3, 'should be on page 3');
+  equal($('#products tr').length, 3, 'should have 2 products in table');
+  equal($('#previous:not(.hidden)').length, 1, 'previous button should not be hidden');
+  equal($('#next.hidden').length, 1, 'next button should be hidden');
+
+  $('#previous').trigger('click');
+  $('#previous').trigger('click');
+
+  equal(db.pagination.currentPage, 1, 'should be on page 1');
+  equal($('#products tr').length, 6, 'should have 5 products in table');
+  equal($('#previous.hidden').length, 1, 'previous button should be hidden');
+  equal($('#next:not(.hidden)').length, 1, 'next button should not be hidden');
+});
+
+
+test('Add Customer', function(){
+  expect(1);
+
+  $('#customer-image').val('ipad-air.png');
+  $('#customer-name').val('Ipad Air');
+  $('#product-weight').val('1.0');
+  $('#product-price').val('500.00');
+  $('#product-off').val('10');
+  $('#add-product').trigger('click');
+
+
+});
+
+
+
+
+
+
+
+function createTestProduct(name, image, weight, price, off){
+  $('#product-image').val(image);
+  $('#product-name').val(name);
+  $('#product-weight').val(weight);
+  $('#product-price').val(price);
+  $('#product-off').val(off);
+  $('#add-product').trigger('click');
+}
+
