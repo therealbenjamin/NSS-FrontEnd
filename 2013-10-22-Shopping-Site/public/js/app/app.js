3b9edb3790d99369a47f94c7ce10c952bf165105
Benjamin Blackburn <benjamin.blackburn@gmail.com>
2013-10-28 08:44:58 -0500

    monday am

--- 2013-10-22-Shopping-Site/public/js/app/app.js
+++ 2013-10-22-Shopping-Site/public/js/app/app.js
@@ -10,13 +10,6 @@ var Δproducts;
 var Δcustomers;
 var Δorders;
 
-// Local Schema (defined in keys.js)
-db.products = db.customers = db.orders = [];
-db.pagination = {};
-db.pagination.perPage = 5;
-db.pagination.currentPage = 1;
-db.pagination.currentRowCount = 0;
-
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
@@ -25,6 +18,7 @@ $(document).ready(initialize);
 
 function initialize(){
   $(document).foundation();
+  initializeSchema();
   initializeDatabase();
   turnHandlersOn();
 }
@@ -32,6 +26,23 @@ function initialize(){
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
+
+function initializeSchema(){
+  db.constants = {};
+  db.constants.domesticShipping = 0.50;
+  db.constants.internationalShipping = 1.50;
+
+  db.products = [];
+  db.customers = [];
+  db.orders = [];
+
+  db.pagination = {};
+  db.pagination.perPage = 5;
+  db.pagination.currentPage = 1;
+  db.pagination.currentRowCount = 0;
+
+  db.cart = new Cart();
+}
 
 function initializeDatabase(){
   Δdb = new Firebase(db.keys.firebase);
@@ -46,14 +57,22 @@ function initializeDatabase(){
 
 function turnHandlersOn(){
   $('#add-product').on('click', clickAddProduct);
+  $('#add-customer').on('click', clickAddCustomer);
   $('#previous').on('click', clickNavigation);
   $('#next').on('click', clickNavigation);
+  $('#products').on('click', 'img', clickAddItemToCart);
+  $('#purchase').on('click', clickPurchase);
+  $('#select-customer').on('change', changeCustomer);
 }
 
 function turnHandlersOff(){
   $('#add-product').off('click');
+  $('#add-customer').off('click');
   $('#previous').off('click');
   $('#next').off('click');
+  $('#products').off('click');
+  $('#purchase').off('click');
+  $('#select-customer').off('change');
 }
 
 // -------------------------------------------------------------------- //
@@ -70,6 +89,16 @@ function clickAddProduct(){
   var product = new Product(image, name, weight, price, off);
   delete product.salePrice;
   Δproducts.push(product);
+}
+
+function clickAddCustomer(){
+  var image = getValue('#customer-image');
+  var name = getValue('#customer-name');
+  var isDomestic = $('input[name="address"]:checked').val() === 'domestic';
+  htmlResetRadioButtons();
+
+  var customer = new Customer(image, name, isDomestic);
+  Δcustomers.push(customer);
 }
 
 function clickNavigation(){
@@ -92,6 +121,33 @@ function clickNavigation(){
   }
 }
 
+function clickAddItemToCart(){
+  if(db.cart.customer){
+    var name = $(this).parent().next().text();
+    var product = _.find(db.products, function(p){return p.name === name;});
+    db.cart.products.push(product);
+    htmlAddItemToCart(product);
+    htmlUpdateCartTotals();
+  }
+}
+
+function clickPurchase(){
+  if(db.cart.customer){
+    var products = _.map(db.cart.products, function(p){return {id: p.id, image: p.image, name: p.name, off: p.off, price: p.price, weight: p.weight};});
+    var order = new Order(null, null, db.cart.customer, products, db.cart.totals.amount(), db.cart.totals.shipping(), db.cart.totals.grand());
+    Δorders.push(order);
+
+    db.cart = new Cart();
+    htmlResetCart();
+  }
+}
+
+function changeCustomer(){
+  var name = this.value;
+  var customer = _.find(db.customers, function(c){return c.name === name;});
+  db.cart.customer = customer;
+}
+
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
@@ -109,11 +165,29 @@ function dbProductAdded(snapshot){
 }
 
 function dbCustomerAdded(snapshot){
-  var customer = snapshot.val();
+  var obj = snapshot.val();
+  var customer = new Customer(obj.image, obj.name, obj.isDomestic);
+  customer.id = snapshot.name();
+  db.customers.push(customer);
+  htmlAddCustomerToSelect(customer);
 }
 
 function dbOrderAdded(snapshot){
-  var order = snapshot.val();
+  var obj = snapshot.val();
+
+  var customer = new Customer(obj.customer.image, obj.customer.name, obj.customer.isDomestic);
+  customer.id = obj.customer.id;
+
+  var products = _.map(obj.products, function(p){
+    var product = new Product(p.image, p.name, p.weight, p.price, p.off);
+    product.id = p.id;
+    return product;
+  });
+
+  var order = new Order(snapshot.name(), obj.time, customer, products, obj.total, obj.shipping, obj.grand);
+
+  db.orders.push(order);
+  htmlAddOrderToTable(order);
 }
 
 // -------------------------------------------------------------------- //
@@ -122,7 +196,7 @@ function dbOrderAdded(snapshot){
 
 function htmlAddProduct(product){
   db.pagination.currentRowCount++;
-  var tr = '<tr class="product"><td class="product-image"><img src="/img/' + product.image + '"></td><td class="product-name">' + product.name + '</td><td class="product-weight">' + product.weight + ' lbs</td><td class="product-price">' + formatCurrency(product.price) + '</td><td class="product-off">' + product.off + '</td><td class="product-sale">' + formatCurrency(product.salePrice()) + '</td></tr>';
+  var tr = '<tr class="product"><td class="product-image"><img src="/img/' + product.image + '"></td><td class="product-name">' + product.name + '</td><td class="product-weight">' + product.weight.toFixed(2) + ' lbs</td><td class="product-price">' + formatCurrency(product.price) + '</td><td class="product-off">' + product.off.toFixed(2) + '</td><td class="product-sale">' + formatCurrency(product.salePrice()) + '</td></tr>';
   var $tr = $(tr);
   $('#products').append($tr);
 }
@@ -139,6 +213,77 @@ function htmlEmptyProductRows(){
   $('.product').remove();
 }
 
+function htmlResetRadioButtons(){
+  $('input[name="address"]:checked')[0].checked = false;
+}
+
+function htmlAddCustomerToSelect(customer){
+  var $option = $('<option>');
+  $option.val(customer.name);
+  $option.text(customer.name);
+  $('#select-customer').prepend($option);
+}
+
+function htmlAddItemToCart(product){
+  var count, $tr, tr;
+  var $tds = $('#cart tbody .product-name');
+  var foundTd = _.find($tds, function(td){return td.innerText === product.name;});
+
+  if(foundTd){
+    count = parseInt($(foundTd).next().text(), 10);
+    count++;
+    $tr = $(foundTd).parent();
+  } else {
+    count = 1;
+    tr = '<tr><td class="product-name"></td><td class="product-count"></td><td class="product-amount"></td><td class="product-weight"></td><td class="product-shipping"></td><td class="product-grand"></td></tr>';
+    $tr = $(tr);
+    $('#cart tbody').append($tr);
+  }
+
+  var amount = product.salePrice() * count;
+  var weight = product.weight * count;
+  var shipping = weight * (db.cart.customer.isDomestic ? db.constants.domesticShipping : db.constants.internationalShipping);
+  var grand = amount + shipping;
+
+  $tr.children('.product-name').text(product.name);
+  $tr.children('.product-count').text(count);
+  $tr.children('.product-amount').text(formatCurrency(amount));
+  $tr.children('.product-weight').text(weight.toFixed(2) + ' lbs');
+  $tr.children('.product-shipping').text(formatCurrency(shipping));
+  $tr.children('.product-grand').text(formatCurrency(grand));
+}
+
+function htmlUpdateCartTotals(){
+  $('#cart-count').text(db.cart.totals.count());
+  $('#cart-amount').text(formatCurrency(db.cart.totals.amount()));
+  $('#cart-weight').text(db.cart.totals.weight().toFixed(2) + ' lbs');
+  $('#cart-shipping').text(formatCurrency(db.cart.totals.shipping()));
+  $('#cart-grand').text(formatCurrency(db.cart.totals.grand()));
+}
+
+function htmlAddOrderToTable(order){
+  var tr = '<tr><td class="order-id"></td><td class="order-time"></td><td class="order-customer"></td><td class="order-products"><ol class="order-products-list"></ol></td><td class="order-total"></td><td class="order-shipping"></td><td class="order-grand"></td></tr>';
+  var $tr = $(tr);
+
+  var $lis = _.map(order.products, function(p){return $('<li>').text(p.name);});
+
+  $tr.children('.order-id').text(order.id);
+  $tr.children('.order-time').text(order.time);
+  $tr.children('.order-customer').text(order.customer.name);
+  $tr.find('.order-products-list').append($lis);
+  $tr.children('.order-total').text(formatCurrency(order.total));
+  $tr.children('.order-shipping').text(formatCurrency(order.shipping));
+  $tr.children('.order-grand').text(formatCurrency(order.grand));
+
+  $('#orders').append($tr);
+}
+
+function htmlResetCart(){
+  $('#select-customer').val('default');
+  $('#cart tbody').empty();
+  $('#cart tfoot td').text('');
+}
+
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
@@ -150,6 +295,34 @@ function Product(image, name, weight, price, off){
   this.price = price;
   this.off = off;
   this.salePrice = function(){return this.price - (this.price * this.off);};
+}
+
+function Customer(image, name, isDomestic){
+  this.image = image;
+  this.name = name;
+  this.isDomestic = isDomestic;
+}
+
+function Cart(){
+  var save = this;
+  this.customer = null;
+  this.products = [];
+  this.totals = {};
+  this.totals.count = function(){return save.products.length;};
+  this.totals.amount = function(){return _.reduce(save.products, function(memo, product){return memo + product.salePrice();}, 0);};
+  this.totals.weight = function(){return _.reduce(save.products, function(memo, product){return memo + product.weight;}, 0);};
+  this.totals.shipping = function(){return this.weight() * (save.customer.isDomestic ? db.constants.domesticShipping : db.constants.internationalShipping);};
+  this.totals.grand = function(){return this.amount() + this.shipping();};
+}
+
+function Order(id, time, customer, products, total, shipping, grand){
+  this.id = id || null;
+  this.time = time || moment().format('MMMM Do YYYY, h:mm:ss a');
+  this.customer = customer;
+  this.products = products;
+  this.total = total;
+  this.shipping = shipping;
+  this.grand = grand;
 }
 
 // -------------------------------------------------------------------- //
--- 2013-10-22-Shopping-Site/public/js/tests/master.js
+++ 2013-10-22-Shopping-Site/public/js/tests/master.js
@@ -5,11 +5,7 @@ module('Integration Testing', {setup: setupTest, teardown: teardownTest});
 function setupTest(){
   turnHandlersOff();
   turnHandlersOn();
-  db.products = [];
-  db.customers = [];
-  db.orders = [];
-  db.pagination.currentPage = 1;
-  db.pagination.currentRowCount = 0;
+  initializeSchema();
   Δdb.remove();
 }
 
@@ -55,22 +51,22 @@ test('Product Pagination', function(){
   }
 
   equal(db.products.length, 12, 'should have 12 products');
-  equal(db.pagination.perPage, 5, 'should have 5 products per page');
-  equal(db.pagination.currentPage, 1, 'should be on page 1');
+  equal(db.pagination.perPage, 5, 'should be 5 products per page');
+  equal(db.pagination.currentPage, 1, 'should be on first page');
   equal($('#products tr').length, 6, 'should have 5 products in table');
   equal($('#previous.hidden').length, 1, 'previous button should be hidden');
   equal($('#next:not(.hidden)').length, 1, 'next button should not be hidden');
 
   $('#next').trigger('click');
 
-  equal(db.pagination.currentPage, 2, 'should be on page 2');
+  equal(db.pagination.currentPage, 2, 'should be on second page');
   equal($('#products tr').length, 6, 'should have 5 products in table');
   equal($('#previous:not(.hidden)').length, 1, 'previous button should not be hidden');
   equal($('#next:not(.hidden)').length, 1, 'next button should not be hidden');
 
   $('#next').trigger('click');
 
-  equal(db.pagination.currentPage, 3, 'should be on page 3');
+  equal(db.pagination.currentPage, 3, 'should be on third page');
   equal($('#products tr').length, 3, 'should have 2 products in table');
   equal($('#previous:not(.hidden)').length, 1, 'previous button should not be hidden');
   equal($('#next.hidden').length, 1, 'next button should be hidden');
@@ -78,38 +74,164 @@ test('Product Pagination', function(){
   $('#previous').trigger('click');
   $('#previous').trigger('click');
 
-  equal(db.pagination.currentPage, 1, 'should be on page 1');
+  equal(db.pagination.currentPage, 1, 'should be on first page');
   equal($('#products tr').length, 6, 'should have 5 products in table');
-  equal($('#previous.hidden').length, 1, 'previous button should be hidden');
-  equal($('#next:not(.hidden)').length, 1, 'next button should not be hidden');
+  ok($('#previous').hasClass('hidden'), 'previous button should be hidden');
+  ok(!$('#next').hasClass('hidden'), 'next button should not be hidden');
 });
-
 
 test('Add Customer', function(){
-  expect(1);
+  expect(7);
 
-  $('#customer-image').val('ipad-air.png');
-  $('#customer-name').val('Ipad Air');
-  $('#product-weight').val('1.0');
-  $('#product-price').val('500.00');
-  $('#product-off').val('10');
-  $('#add-product').trigger('click');
+  $('#customer-image').val('bob.png');
+  $('#customer-name').val('Bob Jenkins');
+  $('#domestic')[0].checked = true;
+  $('#add-customer').trigger('click');
 
+  equal(db.customers.length, 1, 'should have 1 customer in array');
+  ok(db.customers[0] instanceof Customer, 'should be an instance of Customer');
+  equal(db.customers[0].name, 'Bob Jenkins', 'name should be present');
+  equal(db.customers[0].image, 'bob.png', 'image should be present');
+  ok(db.customers[0].id, 'id should be present');
+  ok(db.customers[0].isDomestic, 'should be domestic');
 
+  ok(!$('#domestic')[0].checked, 'domestic should not be checked');
+});
+
+test('Customer DropDown and Shopping Cart', function(){
+  expect(7);
+
+  for(var i = 0; i < 5; i++){
+    var name = Math.random().toString(36).substring(2);
+    var image = Math.random().toString(36).substring(2) + '.png';
+    var isDomestic = _.shuffle([true, false])[0];
+
+    createTestCustomer(name, image, isDomestic);
+  }
+
+  createTestCustomer('Bob', 'bob.png', true);
+
+  // table headers
+  // name, count, amount, weight, shipping, total
+
+  equal(db.customers.length, 6, 'should have 6 customers');
+  equal($('select#select-customer option').length, 7, 'should have 7 option tags');
+  equal($('select#select-customer option:nth-child(1)').val(), 'Bob', 'bob value should be on top of the list');
+  equal($('select#select-customer option:nth-child(1)').text(), 'Bob', 'bob text should be on top of the list');
+  ok($('table#cart').length, 'shopping cart should be visible');
+  equal($('table#cart th').length, 6, 'should be 6 columns');
+  ok($('#purchase').length, 'purchase button should be visible');
+});
+
+test('Add Items to Shopping Cart', function(){
+  expect(19);
+
+  createTestProduct('iPad Air', 'ipad-air.png', 1, 500, 10); // sale - 450
+  createTestProduct('iPhone 5s', 'iphone-5s.png', 0.5, 200, 0); // sale - 200
+  createTestProduct('Apple TV', 'apple-tv.png', 1.5, 100, 5); // sale - 95
+
+  createTestCustomer('Bob', 'bob.png', true);
+  createTestCustomer('Sally', 'sally.png', false);
+
+  $('select#select-customer').val('Sally');
+  $('select#select-customer').trigger('change');
+
+  // 2 iphone 5s
+  $('#products tr:nth-child(3) .product-image img').trigger('click');
+  $('#products tr:nth-child(3) .product-image img').trigger('click');
+
+  // 1 ipad air
+  $('#products tr:nth-child(2) .product-image img').trigger('click');
+
+  // 1 apple tv
+  $('#products tr:nth-child(4) .product-image img').trigger('click');
+
+  equal(db.cart.customer.name, 'Sally', 'shopping cart should belong to sally');
+  ok(db.cart.customer instanceof Customer, 'sally should be a Customer');
+  equal(db.cart.products.length, 4, 'should be 4 items in shopping cart');
+  ok(db.cart.products[0] instanceof Product, 'item in products should be a Product');
+  equal(db.cart.totals.count(), 4, 'should have choosen 4 items');
+  equal(db.cart.totals.amount(), 945, 'amount total should be 945');
+  equal(db.cart.totals.weight(), 3.5, 'weight total should be 3.5');
+
+  // domestic $0.50 lb / international $1.50 lb
+  equal(db.cart.totals.shipping(), 5.25, 'shipping total should be 5.25');
+  equal(db.cart.totals.grand(), 950.25, 'amount total should be 945');
+
+  equal($('#cart thead tr').length, 1, 'should be a header');
+  equal($('#cart tfoot tr').length, 1, 'should be a footer');
+  equal($('#cart tbody tr').length, 3, 'should be 3 items in body');
+
+  equal($('#cart tbody tr:nth-child(1) .product-name').text(), 'iPhone 5s', 'name should be iphone 5s');
+  equal($('#cart tbody tr:nth-child(1) .product-count').text(), '2', 'count should be 2');
+
+  equal($('#cart tfoot tr #cart-count').text(), '4', 'should have 4 items in cart');
+  equal($('#cart tfoot tr #cart-amount').text(), '$945.00', 'should have $945.00 in amount');
+  equal($('#cart tfoot tr #cart-weight').text(), '3.50 lbs', 'should have 3.5 lbs for weight');
+  equal($('#cart tfoot tr #cart-shipping').text(), '$5.25', 'should have $5.25 for shipping');
+  equal($('#cart tfoot tr #cart-grand').text(), '$950.25', 'should have $950.25 for grand');
 });
 
 
+test('Add Order', function(){
+  expect(14);
 
+  createTestProduct('iPad Air', 'ipad-air.png', 1, 500, 10); // sale - 450
+  createTestProduct('iPhone 5s', 'iphone-5s.png', 0.5, 200, 0); // sale - 200
+  createTestProduct('Apple TV', 'apple-tv.png', 1.5, 100, 5); // sale - 95
 
+  createTestCustomer('Bob', 'bob.png', true);
+  createTestCustomer('Sally', 'sally.png', false);
 
+  $('select#select-customer').val('Sally');
+  $('select#select-customer').trigger('change');
 
+  // 2 iphone 5s
+  $('#products tr:nth-child(3) .product-image img').trigger('click');
+  $('#products tr:nth-child(3) .product-image img').trigger('click');
+
+  // 1 ipad air
+  $('#products tr:nth-child(2) .product-image img').trigger('click');
+
+  // 1 apple tv
+  $('#products tr:nth-child(4) .product-image img').trigger('click');
+
+  $('#purchase').trigger('click');
+
+  equal($('#cart tbody tr').length, 0, 'should be no rows in tbody after purchase');
+  equal($('#cart-grand').text(), '', 'should be no grand total after purchase');
+  equal($('#select-customer').val(), 'Select Customer', 'drop down should default after purchase');
+  equal(db.orders.length, 1, 'should be one order after purchase');
+  ok(db.orders[0] instanceof Order, 'should be 1 order instance');
+  ok(db.orders[0].id, 'should have an ID after purchase');
+  equal($('#orders thead th').length, 7, 'should have 7 columns in orders table');
+  equal($('#orders tbody tr').length, 1, 'should have 1 row in orders table body');
+  equal($('#orders tbody .order-time').text().split(' '), 5, 'date time should be formatted');
+  equal($('#orders tbody .order-customer').text(), 'Sally', 'should have customer name');
+  equal($('#orders tbody .order-total').text(), '$945.00', 'should have customer name');
+  equal($('#orders tbody .order-shipping').text(), '$5.25', 'should have customer name');
+  equal($('#orders tbody .order-grand').text(), '$950.25', 'should have customer name');
+  equal($('#orders tbody .order-products-list li'), 4, 'should have 4 items in order');
+});
 
 function createTestProduct(name, image, weight, price, off){
-  $('#product-image').val(image);
   $('#product-name').val(name);
+  $('#product-image').val(image);
   $('#product-weight').val(weight);
   $('#product-price').val(price);
   $('#product-off').val(off);
   $('#add-product').trigger('click');
 }
 
+function createTestCustomer(name, image, isDomestic){
+  $('#customer-name').val(name);
+  $('#customer-image').val(image);
+
+  if(isDomestic){
+    $('#domestic')[0].checked = true;
+  } else {
+    $('#international')[0].checked = true;
+  }
+
+  $('#add-customer').trigger('click');
+}
